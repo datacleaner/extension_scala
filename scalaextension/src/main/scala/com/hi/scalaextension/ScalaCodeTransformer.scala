160c464c0bc7fed08495a6d2ce95a3aaf7d43cac
ak.ankitkumar@gmail.com <ak.ankitkumar@gmail.com>
2012-12-11 21:31:20 +0100

    Scala code transformer

---
+++ scalaextension/src/main/scala/com/hi/scalaextension/ScalaCodeTransformer.scala
@@ -0,0 +1,55 @@
+package com.hi.scalaextension
+
+import org.eobjects.analyzer.beans.api.Transformer
+import org.eobjects.analyzer.beans.api.TransformerBean
+import org.eobjects.analyzer.beans.api.Description
+import org.eobjects.analyzer.beans.api.Categorized
+import org.eobjects.analyzer.beans.categories.StringManipulationCategory
+import org.eobjects.analyzer.data.InputColumn
+import org.eobjects.analyzer.beans.api.Configured
+import org.eobjects.analyzer.beans.api.OutputColumns
+import org.eobjects.analyzer.data.InputRow
+import org.eobjects.analyzer.beans.categories.ScriptingCategory
+import org.eobjects.analyzer.beans.api.StringProperty
+import org.eobjects.analyzer.beans.api.Initialize
+import scala.collection.JavaConversions._
+
+@TransformerBean("Scala Transformer")
+@Description("Supply your own piece of Scala code to do a custom transformation.")
+@Categorized(Array(classOf[ScriptingCategory]))
+class ScalaCodeTransformer(cols: Array[InputColumn[_]]) extends Transformer[java.lang.Object] {
+
+  @Configured
+  @Description("Column to compute string lengths from")
+  var columns: Array[InputColumn[_]] = cols;
+
+  @Configured
+  var returnType : java.lang.Class[_] = classOf[String]
+  
+  def this() = this(null)
+  var cls: java.lang.Class[_] = null
+  var instance: Transformation = null;
+
+  @Configured
+  @Description("Scala scripting code")
+  @StringProperty(multiline = true, mimeType = Array[String] { "text/scala" })
+  var sourceCode: String = "class ScalaTransformer extends com.hi.scalaextension.Transformation {\n\t def execute(map: Map[String, _]) : Any = map(\"A\") \n}";
+  @Initialize
+  def init() {
+    cls = (new Eval).compileCode("ScalaTransformer", sourceCode, true)
+    instance = cls.newInstance().asInstanceOf[Transformation]
+  }
+
+  def getOutputColumns() = { 
+    val columnNames = Array("ScalaOutput")
+    val columnTypes : Array[java.lang.Class[_]] = Array(returnType)
+    new OutputColumns(columnNames, columnTypes)
+  }
+
+  def transform(row: InputRow): Array[java.lang.Object] = {
+    val map : Map[String, _] = columns.map { column => (column.getName(), row.getValue(column)) } toMap
+    val result = instance.execute(map)
+    
+    return Array(result).map{T => T.asInstanceOf[java.lang.Object]}
+  }
+}
---
+++ scalaextension/src/test/scala/com/hi/scalaextension/ScalaCodeTransformerTest.scala
@@ -0,0 +1,49 @@
+package com.hi.scalaextension
+
+import org.junit.Test
+import org.scalatest.junit.AssertionsForJUnit
+import org.scalatest.junit.JUnitSuite
+import org.eobjects.analyzer.data.MockInputColumn
+import org.junit.Assert._
+import org.eobjects.analyzer.data.MockInputRow
+
+class ScalaCodeTransformerTest extends AssertionsForJUnit {
+
+  @Test
+  def testTransform() {
+    val col: MockInputColumn[AnyRef] = new MockInputColumn[AnyRef]("A", classOf[String]);
+
+    val transformer: ScalaCodeTransformer = new ScalaCodeTransformer();
+    transformer.columns = Array(col)
+    transformer.init
+    var result = transformer.transform(new MockInputRow().put(col, "hello"))
+    assertEquals("hello", result(0));
+  }
+
+  @Test
+  def testNumberTransform() {
+    val col: MockInputColumn[AnyRef] = new MockInputColumn[AnyRef]("A", classOf[Integer]);
+
+    val transformer: ScalaCodeTransformer = new ScalaCodeTransformer();
+    transformer.columns = Array(col)
+    transformer.init
+    var result = transformer.transform(new MockInputRow().put(col, 123))
+    assertEquals(123, result(0));
+  }
+  
+  @Test
+  def testScalaIntegerTransform() {
+    val col: MockInputColumn[AnyRef] = new MockInputColumn[AnyRef]("A", classOf[Integer]);
+  
+    val transformer: ScalaCodeTransformer = new ScalaCodeTransformer();
+    transformer.columns = Array(col)
+    transformer.sourceCode = """class ScalaTransformer extends com.hi.scalaextension.Transformation {
+      								def execute(map: Map[String, _]) : Any = 1 
+      							}
+      						 """
+    transformer.init
+    var result = transformer.transform(new MockInputRow().put(col, 123))
+    assertEquals(1, result(0));
+  }
+}
+ 
