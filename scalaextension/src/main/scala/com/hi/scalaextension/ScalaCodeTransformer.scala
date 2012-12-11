4dcb12e48e0302389d7b7110b368475c4a91efcf
ak.ankitkumar@gmail.com <ak.ankitkumar@gmail.com>
2012-12-11 22:01:27 +0100

    Improved the source code to display the Comment to users.

--- scalaextension/src/main/scala/com/hi/scalaextension/ScalaCodeTransformer.scala
+++ scalaextension/src/main/scala/com/hi/scalaextension/ScalaCodeTransformer.scala
@@ -24,8 +24,8 @@ class ScalaCodeTransformer(cols: Array[InputColumn[_]]) extends Transformer[java
   var columns: Array[InputColumn[_]] = cols;
 
   @Configured
-  var returnType : java.lang.Class[_] = classOf[String]
-  
+  var returnType: java.lang.Class[_] = classOf[String]
+
   def this() = this(null)
   var cls: java.lang.Class[_] = null
   var instance: Transformation = null;
@@ -33,23 +33,31 @@ class ScalaCodeTransformer(cols: Array[InputColumn[_]]) extends Transformer[java
   @Configured
   @Description("Scala scripting code")
   @StringProperty(multiline = true, mimeType = Array[String] { "text/scala" })
-  var sourceCode: String = "class ScalaTransformer extends com.hi.scalaextension.Transformation {\n\t def execute(map: Map[String, _]) : Any = map(\"A\") \n}";
+  var sourceCode: String = """
+    class ScalaTransformer extends com.hi.scalaextension.Transformation 
+  	{
+		def execute(map: Map[String, _]) : Any = 
+		/** Replace the below default implementation with your scala scriptlet.
+		*  The arguement map contains key as column name and value as column value.
+		*/ 
+		map("A") 
+  	}""";
   @Initialize
   def init() {
     cls = (new Eval).compileCode("ScalaTransformer", sourceCode, true)
     instance = cls.newInstance().asInstanceOf[Transformation]
   }
 
-  def getOutputColumns() = { 
+  def getOutputColumns() = {
     val columnNames = Array("ScalaOutput")
-    val columnTypes : Array[java.lang.Class[_]] = Array(returnType)
+    val columnTypes: Array[java.lang.Class[_]] = Array(returnType)
     new OutputColumns(columnNames, columnTypes)
   }
 
   def transform(row: InputRow): Array[java.lang.Object] = {
-    val map : Map[String, _] = columns.map { column => (column.getName(), row.getValue(column)) } toMap
+    val map: Map[String, _] = columns.map { column => (column.getName(), row.getValue(column)) } toMap
     val result = instance.execute(map)
-    
-    return Array(result).map{T => T.asInstanceOf[java.lang.Object]}
+
+    return Array(result).map { T => T.asInstanceOf[java.lang.Object] }
   }
 }
